0f13f83b9a0f9fb7362b4aa919abbb97cbfdb67d
uzura22 <aldjad3ufamoe@gmail.com>
2020-12-16 15:18:05 +0900

    画面の一覧

---
+++ jQuery_h.js
@@ -0,0 +1,17 @@
+$("#saisho").on("click", function () {
+    $("#profile_card").show();
+    // $("#juusho_card").hide();
+    // $("#rennrakusaki_card").hide();
+    // $("#gakureki_card").hide();
+    // $("#kamoku_card").hide();
+    // $("#shumi_card").hide();
+    // $("#tokutyou_card").hide();
+    // $("#etakoto_card").hide();
+    // $("#douki_card").hide();
+    // $("#shikaku_card").hide();
+    // $("#bikou_card").hide();
+    // $("#sakuseibi_card").hide();
+    // $("#kakuninn_card").hide();
+    // $("#preview_card").hide();
+});
+// 「はじめから」リンクを押したとき
--- text.js
+++ text.js
@@ -12,106 +12,105 @@
 //     document.getElementById("profile_card")
 //         .style.display = "none";
 
-var juusho_card =
-    document.getElementById("juusho_card")
-        .style.display = "none";
+// var juusho_card =
+//     document.getElementById("juusho_card")
+//         .style.display = "none";
 
-var rennrakusaki_card =
-    document.getElementById("rennrakusaki_card")
-        .style.display = "none";
+// var rennrakusaki_card =
+//     document.getElementById("rennrakusaki_card")
+//         .style.display = "none";
 
-var gakureki_card =
-    document.getElementById("gakureki_card")
-        .style.display = "none";
+// var gakureki_card =
+//     document.getElementById("gakureki_card")
+//         .style.display = "none";
 
-var kamoku_card =
-    document.getElementById("kamoku_card")
-        .style.display = "none";
+// var kamoku_card =
+//     document.getElementById("kamoku_card")
+//         .style.display = "none";
 
-var shumi_card =
-    document.getElementById("shumi_card")
-        .style.display = "none";
+// var shumi_card =
+//     document.getElementById("shumi_card")
+//         .style.display = "none";
 
-var tokutyou_card =
-    document.getElementById("tokutyou_card")
-        .style.display = "none";
+// var tokutyou_card =
+//     document.getElementById("tokutyou_card")
+//         .style.display = "none";
 
-var etakoto_card =
-    document.getElementById("etakoto_card")
-        .style.display = "none";
+// var etakoto_card =
+//     document.getElementById("etakoto_card")
+//         .style.display = "none";
 
-var douki_card =
-    document.getElementById("douki_card")
-        .style.display = "none";
+// var douki_card =
+//     document.getElementById("douki_card")
+//         .style.display = "none";
 
-var shikaku_card =
-    document.getElementById("shikaku_card")
-        .style.display = "none";
+// var shikaku_card =
+//     document.getElementById("shikaku_card")
+//         .style.display = "none";
 
-var bikou_card =
+// var bikou_card =
+//     document.getElementById("bikou_card")
+//         .style.display = "none";
 
-    document.getElementById("bikou_card")
-        .style.display = "none";
+// var sakuseibi_card =
+//     document.getElementById("sakuseibi_card")
+//         .style.display = "none";
 
-var sakuseibi_card =
-    document.getElementById("sakuseibi_card")
-        .style.display = "none";
+// var kakuninn_card =
+//     document.getElementById("kakuninn_card")
+//         .style.display = "none";
 
-var kakuninn_card =
-    document.getElementById("kakuninn_card")
-        .style.display = "none";
+// var preview_card =
 
-var preview_card =
-
-    document.getElementById("preview_card")
-        .style.display = "none";
+//     document.getElementById("preview_card")
+//         .style.display = "none";
 
 // 変数にそれぞれの変数と同じの名のcardのIDを代入、非表示にする
 
-var array_card = new Array(profile_card, juusho_card, rennrakusaki_card, shikaku_card, bikou_card, preview_card);
+// var array_card = new Array(profile_card, juusho_card, rennrakusaki_card, shikaku_card, bikou_card, preview_card);
 
 // 上記の変数を一つの配列に格納する
 
 
 
-function before() {
-    var now_card = document.getElementById('bikou_card');
-    now_card.style.display = 'none';
+// function before() {
+//     var now_card = document.getElementById('bikou_card');
+//     now_card.style.display = 'none';
 
-    var preview_card = document.getElementById('preview_card');
-    preview_card.style.display = 'none';
+//     var preview_card = document.getElementById('preview_card');
+//     preview_card.style.display = 'none';
 
-    var next_card = document.getElementById('profile_card');
-    next_card.style.display = 'block';
-}
+//     var next_card = document.getElementById('profile_card');
+//     next_card.style.display = 'block';
+// }
 
 // 「前へ」ボタンを押したとき
 
-function next() {
-    var now_card = document.getElementById('profile_card');
-    now_card.style.display = 'none';
+// function next() {
+//     var now_card = document.getElementById('profile_card');
+//     now_card.style.display = 'none';
 
-    var preview_card = document.getElementById('preview_card');
-    preview_card.style.display = 'none';
+//     var preview_card = document.getElementById('preview_card');
+//     preview_card.style.display = 'none';
 
-    var next_card = document.getElementById('bikou_card');
-    next_card.style.display = 'block';
-}
+//     var next_card = document.getElementById('bikou_card');
+//     next_card.style.display = 'block';
+// }
 
 // 「次へ」ボタンを押したとき
 
-function preview() {
-    var now_card = document.getElementById('profile_card');
-    now_card.style.display = 'none';
+// function preview() {
+//     var now_card = document.getElementById('profile_card');
+//     now_card.style.display = 'none';
 
-    var preview_card = document.
-        getElementById('preview_card');
-    preview_card.style.display = 'block';
+//     var preview_card = document.
+//         getElementById('preview_card');
+//     preview_card.style.display = 'block';
 
-    var next_card = document.
-        getElementById('bikou_card');
-    next_card.style.display = 'none';
-}
+//     var next_card = document.
+//         getElementById('bikou_card');
+//     next_card.style.display = 'none';
+// }
 
 // getElementByIdとは、指定したid値を持つ要素をElementオブジェクトとして返すメソッドです。
 
@@ -127,7 +126,5 @@ function preview() {
 // https://allabout.co.jp/gm/gc/23973/#aa23973display
 
 
-$(function () {
-    $("#cover").load("preview.html");
-});
+
 
